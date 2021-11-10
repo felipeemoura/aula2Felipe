fb766ed427bf255274d8b42ef4b7792ed67098fd
kynsh <feliphsantoos@gmail.com>
2021-11-10 18:06:23 -0300

    lembrei agora

---
+++ string.js
@@ -0,0 +1,36 @@
+//concatenar
+
+/*console.log('Hello' + ' ' + "World");
+
+console.log('Hello' + "World");
+
+myName = "Felipe";
+
+console.log("Hello" +' '+ myName);
+
+myName = "Felipe";
+console.log(`Hello ${myName}`);*/
+
+//--------------------
+
+/*const x = 0
+y = "apple"
+console.log(x,"\n",y)*/
+
+//--------------------
+
+const input = require("readline-sync");
+const mes = input.question("Digite um mes")
+
+if (mes === "fevereiro") {
+    console.log("TEM CARNAVAL")
+} else if (mes === "junho") {
+    console.log("TEM SÃO JOÃO")
+} else if (mes === "dezembro"){
+    console.log("MÊS DO GORDINHO DA BARBA BRANCA")
+} else {
+    console.log(`${mes} não é um mês festivo`)
+}
+
+
+
