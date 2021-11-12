63061e4a9cbd5fd66ee06aed5be5b5b10613bbe7
kynsh <93688312+kynsh@users.noreply.github.com>
2021-11-11 21:19:57 -0300

    for-var

---
+++ aula3/training.js
@@ -0,0 +1,46 @@
+/*let palavras = "comi ovo de manhã cedo";
+
+console.log(palavras.length);*/
+
+//----------------
+
+/*var senha = "ahsgdkak"
+
+if (senha.length == 8) {
+    console.log("Tudo ok!")
+} else {
+    console.log("Senha inválida")
+}*/
+
+/*let salario = 1000
+while (salario <5000) {
+    salario += 100;
+    console.log("O salário ainda é R$"+ salario);
+}*/
+
+/*let contador = 0
+while (contador <100) {
+    console.log("repetição n"+ contador);
+    contador++
+}*/
+
+
+/*const linguagens = ['JavaScript', 'Python', 'c#', 'PHP'];
+let index = 0
+
+do {
+    console.log(linguagens[index])
+    index++
+}while (index < linguagens.length)*/
+
+
+
+/*for (var i = 0; i < 9; i++) {
+    console.log(i);
+}*/
+
+
+for (var i = 0;; i++){
+    console.log(i)
+    if (i>100);
+}
